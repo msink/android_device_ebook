d9383502812943448034a2f08ebeea15ff447c34
Mike Sinkovsky <msink@permonline.ru>
2015-02-22 14:55:43 +0500

    onyx maxwell 20150116

--- onyx_rk29/onyx_rk29.mk
+++ onyx_rk29/onyx_rk29.mk
@@ -4,6 +4,7 @@ PRODUCT_LOCALES := \
 	ar ar_EG cs_CZ da_DK de_DE el_GR en_GB en_US es_ES es_US fr_FR it_IT \
 	iw iw_IL ja_JP ko_KR nb_NO nl_NL pl_PL pt_BR pt_PT ru_RU sv_SE tr_TR \
 	zh_CN zh_TW mdpi hdpi 1024x758 800x600
+PRODUCT_LOCALES += hu hu_HU 1200x825
 
 PRODUCT_PACKAGES +=
 PRODUCT_COPY_FILES +=
